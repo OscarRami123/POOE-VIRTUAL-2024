2401fda887bb4610ea7d83a0157384ca9fa3e356
OSCAR RENE RAMIREZ MENA <DSNP209523@ugb.edu.sv>
2024-09-17 20:44:16 -0600

    Mayor de 3 numeros

--- miPrimerProyecto/miPrimerProyecto/Module1.vb
+++ miPrimerProyecto/miPrimerProyecto/Module1.vb
@@ -1,7 +1,26 @@
 ﻿Module Module1
 
     Sub Main()
-        Console.WriteLine("Bienvenido chicos de programación orientada a objetos y eventos")
+        Dim num1, num2, num3, mayor As Integer
+        Console.Write("Num 1: ")
+        num1 = Console.ReadLine()
+
+        Console.Write("Num 2: ")
+        num2 = Console.ReadLine()
+
+        Console.Write("Num 3: ")
+        num3 = Console.ReadLine()
+
+        If num1 > num2 Then
+            mayor = num1
+        Else
+            mayor = num2
+        End If
+        If num3 > mayor Then
+            mayor = num3
+        End If
+        Console.WriteLine("El mayor de los 3 numeros es : {0}", mayor)
+
         Console.ReadKey()
     End Sub
 
